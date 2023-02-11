a845733f6cfc8e5f9afdabb5a78f5d710803fb00
RaniSales <Ranievly5@hotmail.com>
2023-02-11 00:52:13 -0300

    component-NavBar

---
+++ pokedex-frontend/src/Components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NavBar = () => {
+    const logo = "https://pokedex-grupo10.surge.sh/static/media/lgo.5511e54e001107cf3e5a.png"
+    return (
+        <nav>
+            <div>
+                <img
+                    alt="logo-pokedex"
+                    src={logo}
+                    className="navBar-img"
+                />
+            </div>
+        </nav>
+    );
+};
+export default NavBar;
