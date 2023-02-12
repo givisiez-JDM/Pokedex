cfd1c6e30fce55168ace859f13957238f6820fd7
RaniSales <Ranievly5@hotmail.com>
2023-02-12 14:07:24 -0300

    resolving conflict

--- pokedex-frontend/src/App.js
+++ pokedex-frontend/src/App.js
@@ -1,26 +1,21 @@
-<<<<<<< HEAD
-import "./App.css";
-import NavBar from "./Components/NavBar/NavBar";
-import SearchPokemon from "./Components/SearchPokemon/SearchPokemon";
-=======
+
 import React from "react";
 import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import SearchBar from "./Components/SearchPokemon/SearchPokemon";
 import Pokedex from "./Components/Pokedex/Pokedex";
->>>>>>> 686beaf0612ab641044ceb642871d68b166a58c2
+import SearchPokemon from "./Components/SearchPokemon/SearchPokemon";
 
 function App() {
   return (
     <div>
       <NavBar />
-<<<<<<< HEAD
+
       <SearchPokemon />
-=======
       <SearchBar />
       <Pokedex />
       <div className="App"></div>
->>>>>>> 686beaf0612ab641044ceb642871d68b166a58c2
+
     </div>
   );
 }
