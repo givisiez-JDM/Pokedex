e7af2eadb9f573efb092cb289c0bf0de51af228f
RaniSales <Ranievly5@hotmail.com>
2023-02-13 08:52:38 -0300

    resolving conflicts

--- pokedex-frontend/src/App.js
+++ pokedex-frontend/src/App.js
@@ -1,43 +1,9 @@
-<<<<<<< HEAD
-import React, {useEffect, useState} from "react";
-=======
 
 import React, {useEffect,useState} from "react";
->>>>>>> f3d14250b05f11cb44c91c3c4844bf0791044466
 import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import SearchBar from "./Components/SearchBar/SearchBar";
 import Pokedex from "./Components/Pokedex/Pokedex";
-<<<<<<< HEAD
-import { getPokemons } from "./api";
-
-
-function App() {
-
-  const [loading, setLoading] = useState(false)
-  const [pokemons, setPokemons] = useState([])
-
-  const fetchPokemons = async () => {
-    try{
-      setLoading(true)
-      const result = await getPokemons();
-      setPokemons(result);
-      setLoading(false)
-    }catch(error){
-      console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    fetchPokemons()
-  })
-
-  return (
-    <div>
-      <NavBar />
-      <SearchBar />
-      <Pokedex pokemons={pokemons} loading={loading}/>
-=======
 import { getPokemonData, getPokemons, searchPokemon } from "./api";
 
 
@@ -88,7 +54,6 @@ function App() {
       <SearchBar onSearch={onSearchHandler}/>
       <Pokedex  pokemon={pokemon}
           loading={loading}/>
->>>>>>> f3d14250b05f11cb44c91c3c4844bf0791044466
       <div className="App"></div>
 
     </div>
