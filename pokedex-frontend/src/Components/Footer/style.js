b94f1022d790054954cbaf2ba743c56d901c0aea
RaniSales <Ranievly5@hotmail.com>
2023-02-21 22:50:37 -0300

    footer

--- pokedex-frontend/src/Components/Footer/style.js
+++ pokedex-frontend/src/Components/Footer/style.js
@@ -2,20 +2,23 @@ import styled from "styled-components";
 
 export const FooterContainer = styled.div`
     display: flex;
+    justify-content:center;
     width: 100%;
     height: 10vh;
     background-color: #121212;
     color: white;
     font-size: 1em;
     border: none;
+    margin: 0 auto;
+    position:absolute;
 `
 
 export const InformationsContainer = styled.div`
     display: flex;
     align-items: center;
     text-align: center;
-    justify-content: center;
     height: 100%;
-    margin: auto;
+    margin:0 auto;
+    position: fixed;
 `
 
